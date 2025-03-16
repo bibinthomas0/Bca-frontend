bfcaa78a0e01a5b338a7ae61feddda25efacfdb7
Bibin Thomas <bibin.thomas@acelrtech.com>
2025-03-16 19:27:05 +0530

    Search fix

--- src/Pages/User/Buyer/BuyerHomePage.js
+++ src/Pages/User/Buyer/BuyerHomePage.js
@@ -73,28 +73,29 @@ const BuyerHomePage = () => {
     const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await apiClient.get("/api/products/");
-        setProducts(response.data);
-      } catch (err) {
-        setError("Failed to load products.");
-      } finally {
-        setLoading(false);
-      }
-    };
+ 
 
     fetchProducts();
   }, []);
-
+  
+  const fetchProducts = async () => {
+    try {
+      const response = await apiClient.get("/api/products/");
+      setProducts(response.data);
+    } catch (err) {
+      setError("Failed to load products.");
+    } finally {
+      setLoading(false);
+    }
+  };
   const handleSearch = () => {
     if (!searchTerm) {
-      setSuggestions([]);
+      fetchProducts() 
     } else {
       const filtered = products.filter((product) =>
         product.title.toLowerCase().includes(searchTerm.toLowerCase())
       );
-      setSuggestions(filtered);
+      setProducts(filtered)
     }
   };
   const handleCreateEnquiry = async () => {
